be44192bb42aaf30cd18e7117e79f793cca4832e
zcc <listenzcc@mail.bnu.edu.cn>
2019-05-28 14:37:06 +0800

    modify psychtoolbox_demo, add play movies

--- psychtoolbox_demo.py
+++ psychtoolbox_demo.py
@@ -1,12 +1,35 @@
+# coding: utf-8
+
+import os
+import time
 from psychopy import visual, core, event
 
+
 win = visual.Window()
+
 msg = visual.TextStim(win, text=u"\u00A1Hola mundo!")
 
 msg.draw()
 win.flip()
 core.wait(1)
 
+n = 117
+s = 4/(n-1)
+imgs = [visual.ImageStim(win, image=os.path.join(
+    'movie_4D', 'pics', '%s_%d.png' % ('quzhou', j))) for j in range(n)]
+
+t = time.time()
+for j, img in enumerate(imgs):
+    img.draw()
+    win.flip()
+    while (time.time()-t) < s*j:
+        pass
+
+print(time.time()-t)
+
+event.waitKeys()
+
+'''
 msg = visual.TextStim(win, text='waitKeys begins.')
 msg.draw()
 win.flip()
@@ -18,9 +41,12 @@ while True:
     if key == ['escape']:
         break
 
-core.wait(3)
-msg = visual.TextStim(win, text='getKeys begins.')
-keys = event.getKeys(timeStamped=True)
-print(keys)
-
+for j in range(3, 0, -1):
+    msg = visual.TextStim(win, text='Close in %d seconds.' % j)
+    msg.draw()
+    win.flip()
+    core.wait(1)
+    keys = event.getKeys(timeStamped=True)
+    print(keys)
+'''
 win.close()
